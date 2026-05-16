b10aee68c7e8af0cb472a3f2732b6dd911a7e55d
heliopr <helioprorep@gmail.com>
2026-05-16 20:49:33 -0300

    feat: adicionar classe Elemento

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
+#include <iostream>
+
 #include "Jogo.h"
+#include "Listas/Lista.h"
 
 int main() {
+    int x = 10;
+    int y = -100;
+
+    Listas::Elemento<int> a;
+    a.incluir(&x);
+
+    Listas::Elemento<int> b;
+    b.incluir(&y);
+    a.setProx(&b);
+
+    std::cout << *a.getInfo() << std::endl;
+    std::cout << *(a.getProximo()->getInfo()) << std::endl;
+    std::cout << (a.getProximo()->getProximo()) << std::endl;
+
     Jogo jogo;
     jogo.executar();
     return 0;
