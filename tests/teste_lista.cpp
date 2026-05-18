681bb7668ad37459e917969f9f6622ef86ad6ce7
heliopr <helioprorep@gmail.com>
2026-05-18 11:36:18 -0300

    test: adicionar testes da Lista

--- tests/teste_lista.cpp
+++ tests/teste_lista.cpp
@@ -29,10 +29,54 @@ void teste_incluir() {
     std::cout << "OK teste_incluir" << std::endl;
 }
 
+void teste_limpar() {
+    Lista<int> lista;
+
+    int v[10];
+
+    for (int i = 0; i < 10; i++) {
+        v[i] = i;
+        lista.incluir(&v[i]);
+    }
+
+    lista.limpar();
+    assert(lista.vazia() == true);
+    
+    std::cout << "OK teste_limpar" << std::endl;
+}
+
+void teste_incluir_varios() {
+    Lista<int> lista;
+    int num = 1000;
+
+    int *v = new int[num];
+    for (int i = 0; i < num; i++) {
+        v[i] = i;
+        lista.incluir(&v[i]);
+    }
+
+    Lista<int>::Elemento *atual = lista.getPrimeiro();
+    int cont = 0;
+    while (atual != 0) {
+        cont++;
+        atual = atual->getProximo();
+    }
+
+    assert(cont == num);
+
+    lista.limpar();
+    
+    delete[] v;
+
+    std::cout << "OK teste_incluir_varios" << std::endl;
+}
+
 int main() {
 
     teste_lista_vazia();
     teste_incluir();
+    teste_limpar();
+    teste_incluir_varios();
 
     std::cout << "PASSOU teste_lista.cpp" << std::endl;
     return 0;
